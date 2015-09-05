2075ad86db07f4dae60bf1a3b822489669114a6d
Adrian Frith <adrian@adrianfrith.com>
2015-09-06 01:48:02 +0200

    add sheet info layer

--- ngi-tiles.js
+++ ngi-tiles.js
@@ -1,5 +1,3 @@
-// Map
-
 var map = L.map('map', {
   //center: [-28.5, 24.5],
   //zoom: 6,
@@ -8,8 +6,6 @@ var map = L.map('map', {
   //maxBounds: [[-34.85, 16.25], [-22, 33]]
 });
 map.fitBounds([[-34.85, 16.25], [-22, 33]]);
-
-// Layers
 
 var fiftyk = L.tileLayer('tiles/{z}/{x}/{y}.png', {
   tms: true,
@@ -26,14 +22,34 @@ var graticule = L.layerGroup([
   L.graticule({interval: 1, style: { weight: 2 }})
 ]);
 
-// Controls
-
-L.control.layers({
+var layerControl = L.control.layers({
   "1:50 000 topo": fiftyk
 }, {
   "OpenStreetMap": osm,
   "Graticule": graticule
 }).addTo(map);
+
+$.getJSON('sheet50k.json', function (data) {
+  var sheets = L.geoJson(data, {
+    style: {
+      stroke: true, color: 'black', weight: 2, opacity: 1,
+      fill: true, fillColor: 'lightblue', fillOpacity: 0
+    },
+    onEachFeature: function (feature, layer) {
+      var props = feature.properties;
+      layer.bindPopup('Sheet ' + props.s + '<br/>Ed. ' + props.e + ' (' + props.y + ')');
+
+      layer.on('mouseover', function () {
+        this.setStyle({ fillOpacity: 0.5 });
+      });
+      layer.on('mouseout', function () {
+        this.setStyle({ fillOpacity: 0 });
+      });
+    }
+  });
+
+  layerControl.addOverlay(sheets, "Sheet info");
+});
 
 var hash = new L.Hash(map);
 
